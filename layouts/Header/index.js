1aadee3def1eaf3919314676af9c56f9408424c8
aomine <aomine1745@gmail.com>
2020-11-17 22:44:05 -0500

    header layout

--- layouts/Header/index.js
+++ layouts/Header/index.js
@@ -1,8 +1,16 @@
+import Avatar from 'components/Avatar'
+
 import styles from './index.module.css'
 
 export default function Header () {
   return (
     <header className={styles.header}>
+      <div className={styles.avatar}>
+      <Avatar
+        src="https://avatars1.githubusercontent.com/u/40266562?v=4"
+        size='30'
+      />
+      </div>
       <h2 className={styles.title}>Inicio</h2>
     </header>
   )
